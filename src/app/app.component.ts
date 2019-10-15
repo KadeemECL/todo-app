1b0e0ae2cd252c8e8e5a6023fa5d83cbeb35c5e3
Kadeem Clarke Legg <Kadeemcl@gmail.com>
2019-10-14 23:57:30 -0400

    Uploaded the app onto Github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Angular To Do List';
+  todos = [
+      {
+      label: 'Bring Milk '
+    },
+    {
+      label: 'Do Homework ',
+    },
+    {
+      label: 'Clean House ',
+    },
+    {
+      label: 'Wash Car ',
+    }
+  ];
+
+  addTodo(newTodoLabel) {
+    const newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+      done: false,
+    };
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo) {
+    this.todos = this.todos.filter( t => t.label !== todo.label);
+  }
 }
